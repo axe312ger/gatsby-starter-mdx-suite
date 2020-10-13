b9ae110685bb8c87e8f9fdfd707666bdb7131450
Benedikt Rötsch <axe312ger@users.noreply.github.com>
2020-10-13 11:10:18 +0200

    style: use default gatsby eslint config and apply it (#7)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,6 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  plugins: ['react'],
   globals: {
-    graphql: false,
+    __PATH_PREFIX__: true,
   },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: '2017',
-  },
-  parser: 'babel-eslint',
-  extends: ['standard', 'standard-react', 'prettier'],
-  rules: {
-    'import/no-unresolved': [2, { commonjs: true }],
-    'react/jsx-pascal-case': [2, { allowAllCaps: true }],
-  },
+  extends: `react-app`,
 }
